70ac086686056c54cb147dce8623d967601d0a85
Callum <groegercesg@gmail.com>
2017-12-31 22:20:51 +0000

    Nightly push of slight modifications to the main.py file

--- main.py
+++ main.py
@@ -19,30 +19,40 @@ def UsrOpt1():
     #Data Collection#
     #################
     #File Collection number count
-    count = int(00000)
+    count = int(000)
     #number cycle iteration thingy
+    for count in range(999):
 
+        devID=0 #Device number, this being the number, in classic array style, of sensors plugged into the computer
+        acqTime=1 #Time between Acquisitions, measured in seconds
+        acqCount=10 #Number of Acquisition
+        for i in range(10):
+            #
+            name = 'TPIX-' + count
+            dir_name = os.path.join('C:\\Users\\XXX\\Documents\\TaptonPIX\\TPIX', name)
+            fileName1 = dir_name  "c:\\folder\\rep%02d_.txt" % i
+            pixelman.mpxCtrlPerformFrameAcq(devID, acqCount, acqTime, pixelman.FSAVE_ASCII | pixelman.FSAVE_SPARSEX, fileName)
 
     ################
     #Data Packaging#
     ################
 
 
-    #This is the line that grabs the data from a certain location and packes them up
-    #Zip Name
-    zpname = 'TPIX-' + count + '.zip'
-    #Joining the two directory components together, the location and the File Name
-    dir_name = os.path.join('C:\\Users\\XXX\\Documents\\TaptonPIX\\TPIX', zpname)
-    Imaginary_zip = zipfile.ZipFile(dir_name, 'w')
+        #This is the line that grabs the data from a certain location and packes them up
+        #Zip Name
+        zname = 'TPIX-' + count + '.zip'
+        #Joining the two directory components together, the location and the File Name
+        dir_name_z = os.path.join('C:\\Users\\XXX\\Documents\\TaptonPIX\\TPIX', zname)
+        Imaginary_zip = zipfile.ZipFile(dir_name_z, 'w')
 
-    #Change it here as well
-    for folder, subfolders, files in os.walk('C:\\Users\\XXX\\Documents\\TaptonPIX'):
+        #Change it here as well
+        for folder, subfolders, files in os.walk('C:\\Users\\XXX\\Documents\\TaptonPIX'):
 
-        for file in files:
-            if file.endswith('.txt.dsc') and file.endswith('.txt'):
-                Imaginary_zip.write(os.path.join(folder, file), file, compress_type = zipfile.ZIP_DEFLATED)
+            for file in files:
+                if file.endswith('.txt.dsc') and file.endswith('.txt'):
+                    Imaginary_zip.write(os.path.join(folder, file), file, compress_type = zipfile.ZIP_DEFLATED)
 
-    Imaginary_zip.close()
+        Imaginary_zip.close()
 
     ##############
     #Data Sending#
@@ -52,30 +62,30 @@ def UsrOpt1():
 
 
 
-    file_path = dirname  # The path to the ZIP file you want to upload. Make sure it exists!
+        file_path = dirname  # The path to the ZIP file you want to upload. Make sure it exists!
 
-    # Use the following when uploading to TAPAS, not the local development site
-    API_BASE_URL = "https://tapas.researchinschools.org/"
+        # Use the following when uploading to TAPAS, not the local development site
+        API_BASE_URL = "https://tapas.researchinschools.org/"
 
-    #
-    headers = {
-        'Authorization': 'Token 5e865d620e7cc43ddb3b7ff8e3ee5728f27b258e',  # You MUST include the 'Token' part here before the API token
-    }
+        #
+        headers = {
+            'Authorization': 'Token 5e865d620e7cc43ddb3b7ff8e3ee5728f27b258e',  # You MUST include the 'Token' part here before the API token
+        }
 
-    #Fill out the payload fields,
-    payload = {
-        'name': 'Name of Upload',  # Name of the upload, can be whatever you like, but make it meaningful
-        'project': '1',  # You MUST specify a project in ID form here!
-                         # You can check all project IDs via the API too :)
-        'latitude': '1',  # Latitude of where data was taken, needed if you choose RAY as a project
-        'longitude': '1',  # Longitude of where data was taken, needed if you choose RAY as a project
-    }
+        #Fill out the payload fields,
+        payload = {
+            'name': 'Name of Upload',  # Name of the upload, can be whatever you like, but make it meaningful
+            'project': '1',  # You MUST specify a project in ID form here!
+                             # You can check all project IDs via the API too :)
+            'latitude': '1',  # Latitude of where data was taken, needed if you choose RAY as a project
+            'longitude': '1',  # Longitude of where data was taken, needed if you choose RAY as a project
+        }
 
-    # The / at the end of upload is VERY important here! You'll get an Internal Server Error without it.
-    # .... I speak from experience
-    r = requests.post(API_BASE_URL + "/api/v1/upload/", files={"zipfile": open(file_path, 'rb')}, data=payload, headers=headers)
+        # The / at the end of upload is VERY important here! You'll get an Internal Server Error without it.
+        # .... I speak from experience
+        r = requests.post(API_BASE_URL + "/api/v1/upload/", files={"zipfile": open(file_path, 'rb')}, data=payload, headers=headers)
 
-    print(r.text)
+        print(r.text)
 
 
 #Number 2 allows the user to edit any of the options necessary; <insert options here>
